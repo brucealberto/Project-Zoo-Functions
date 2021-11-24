dddae05aa098f47eac5475365866c32f3de12e59
brucealberto <brucealberto2@gmail.com.br>
2021-11-24 15:25:27 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,17 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const pegaEmpregado = employees.find(
+    (employe) =>
+      employeeName.includes(employe.firstName)
+      || employeeName.includes(employe.lastName),
+  );
+  return pegaEmpregado;
 }
 
 module.exports = getEmployeeByName;
