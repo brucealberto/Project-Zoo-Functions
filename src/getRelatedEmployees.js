c99af1fb1fe7d8c0eb133c3a12e97cc67ed1f2a7
brucealberto <brucealberto2@gmail.com.br>
2021-11-24 15:35:53 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const pessoaGerente = employees
+    .filter((employee) => employee.managers.includes(managerId))
+    .map((employee) => `${employee.firstName} ${employee.lastName}`);
+  return pessoaGerente;
 }
 
 module.exports = { isManager, getRelatedEmployees };
