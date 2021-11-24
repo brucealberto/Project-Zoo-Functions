fd3fef325518f92d8dc5ad61e7cd65bba8ae4e39
brucealberto <brucealberto2@gmail.com.br>
2021-11-24 15:12:09 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+
+  const animalVelho = species
+    .filter((specie) => specie.name === animal)[0]
+    .residents.every((resident) => resident.age > age);
+  return animalVelho;
+  // return pegaAnimal[0].residents.every( (resident) => resident.age > age)
 }
 
 module.exports = getAnimalsOlderThan;
