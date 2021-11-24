a1d768a716a8fd34bb428d06c87ab45f22b2cb43
brucealberto <brucealberto2@gmail.com.br>
2021-11-24 14:57:50 -0300

    correção lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,9 @@
-const data = require("../data/zoo_data");
+const data = require('../data/zoo_data');
 
 function getSpeciesByIds(id1, id2) {
   // seu código aqui
   const buscaEpecies = data.species.filter(
-    (specie) => specie.id === id1 || specie.id === id2
+    (specie) => specie.id === id1 || specie.id === id2,
   );
   return buscaEpecies;
 }
