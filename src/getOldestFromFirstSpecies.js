961e700144f928c7bdd8589a9efa9451b5a99747
brucealberto <brucealberto2@gmail.com.br>
2021-11-25 17:35:07 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,23 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+function primeiroFunc(id) {
+  const procuraFunc = employees.find((employee) => employee.id === id);
+  const firstAnimal = procuraFunc.responsibleFor[0];
+  return firstAnimal;
+}
+
+function primeiroAnimalSpecie(idAnimal) {
+  const procuraAnimal = species.find((specie) => specie.id === idAnimal);
+  const residents = procuraAnimal.residents.sort((a, b) => b.age - a.age)[0];
+  return residents;
+}
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const idFirstAnimal = primeiroFunc(id);
+  const objAnimal = primeiroAnimalSpecie(idFirstAnimal);
+  return Object.values(objAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
